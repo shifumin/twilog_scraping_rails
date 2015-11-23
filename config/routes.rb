756e58c654a1b405c303d939835275ef51914ca6
shifumin <shifumin4230@gmail.com>
2015-11-23 12:47:57 +0000

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :tweets, only: [:create, :new, :show]
+  # resources :tweets, only: [:create, :new, :show]
+  resources :tweets, only: [:create, :new]
+  post 'tweets/show'
   root 'tweets#new'
 end
